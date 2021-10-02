e94da2f4990bc35e871f7faa4b637e5d87bb75d2
unknown <dasdipak98047@gmail.com>
2021-10-02 16:36:33 +0545

    worker dashboard

---
+++ app/src/main/java/com/app/cementfactory/ui/ManagerDashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.app.cementfactory.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.cementfactory.R
+
+class ManagerDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manager_dashboard)
+    }
+}
---
+++ app/src/main/java/com/app/cementfactory/ui/WorkerDashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.app.cementfactory.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.cementfactory.R
+
+class WorkerDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker_dashboard)
+    }
+}
